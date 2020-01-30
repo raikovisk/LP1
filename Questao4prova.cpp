7e99e35446887c5377f2907de189f4b47a55ae67
raikovisk <58261944+raikovisk@users.noreply.github.com>
2020-01-29 22:33:55 -0300

    Update Questao4prova.cpp

--- Questao4prova.cpp
+++ Questao4prova.cpp
@@ -3,7 +3,7 @@
 	main() {
 	int t[4]; //Tempos de prova
 	int P[4]; // Etapas
-	int ins = 0, vencedor, nvencedor, i, Tpadrao, pontos = 0;
+	int ins = 0, vencedor = 0, nvencedor, i, Tpadrao, pontos;
 	float Delta;
 	printf ("\n\t  ------| Classificacao Toneio de Rally |------ \t\n");
 	printf("\n Informe o tempo padrao da prova(em minutos): ");
@@ -15,33 +15,35 @@
 		if (ins != 9999)
 		{
 		system("cls");	
+		pontos = 0;
 		for(i=1;i<=3;i++){
-			vencedor = 0;
 			printf("\n %do tempo(em minutos) da Equipe %d: ",i,ins);
 			scanf("%d",&t[i]);
 			Delta = (t[i]-Tpadrao);
 			if(Delta < 3){
 				P[i] = 100;
+				pontos = pontos+P[i];
 				}
 			else if((Delta >= 3) && (Delta <= 5)){
 				P[i] = 80;
+				pontos = pontos+P[i];
 				}
 			else if (Delta > 5){
 			P[i] = 80-(Delta-5)/5;
-			}
 			pontos = pontos+P[i];
+			}
 		}
 		if (vencedor < pontos){
 			vencedor = pontos;
 			nvencedor = ins;
 			}
-	}
-	printf ("\n ----------------- \n");		
-	printf ("\n Equipe: %d",ins);
-	printf ("\n Pontos da 1a Etapa %d \n Pontos da 2a Etapa %d \n Pontos da 3a Etapa %d",P[1],P[2],P[3]);
-	printf ("\n Total de Pontos Obtidos: %d \n",pontos);
-	printf ("\n Obs: inscricao 9999 para Sair");
-	printf ("\n ----------------- \n");
-	}
+		printf ("\n ----------------- \n");		
+		printf ("\n Equipe: %d",ins);
+		printf ("\n Pontos da 1a Etapa %d \n Pontos da 2a Etapa %d \n Pontos da 3a Etapa %d",P[1],P[2],P[3]);
+		printf ("\n Total de Pontos Obtidos: %d \n",pontos);
+		printf ("\n Obs: inscricao 9999 para Sair");
+		printf ("\n ----------------- \n");
+		}
+	}	
 	printf("\n A equipe vencedora foi %d com %d pontos \n", nvencedor, vencedor);	
 }
